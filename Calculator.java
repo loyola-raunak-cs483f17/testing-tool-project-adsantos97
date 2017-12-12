ef4a00e22a65bbeffdd8530cc583ea47e516380e
adsantos97 <adsantos@loyola.edu>
2017-12-12 01:17:27 -0500

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Calculator {
+	public String sum(int a, int b) {
+		return String.valueOf(a+b);
+	}
+	
+	public String product(int a, int b) {
+		return String.valueOf(a*b);
+	}
+}
